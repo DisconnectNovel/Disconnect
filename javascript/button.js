f866ec6a6515f3b15c83040032caccdd7ac481d7
Mike Bardet <mike.bardet@epfl.ch>
2018-04-28 00:18:32 +0200

    Add transitions

--- javascript/button.js
+++ javascript/button.js
@@ -1,4 +1,28 @@
 
+var social_score = 10
+var civil_score = 10
+var selected = 0
+
+function process(social_increment, civil_increment, increment) {
+    //alert(String(social_increment) + " " + String(civil_increment) + " " + String(increment))
+    selected += increment
+    social_score += social_increment
+    civil_score += civil_increment
+
+    document.getElementById("social_score").innerHTML = social_score
+    document.getElementById("civil_score").innerHTML = civil_score
+
+    // Put all the images as transparent
+    $("#fading img").removeClass("opaque");
+    // Put only the selected image as opaque
+    $("#fading img").eq(selected).addClass("opaque");
+
+    // Same with the form
+    $("#forms div").removeClass("opaque");
+    $("#forms div").eq(selected).addClass("opaque");
+}
+
+/*
 function initialize() {
     localStorage.setItem("social_score", 10);
     localStorage.setItem("civil_score", 10);
@@ -18,4 +42,4 @@ function process(social_increment, civil_increment, next_page) {
 function go_to_page(next_page) {
     localStorage.setItem("warn_before_leaving_page", "false")
     window.location.href = next_page;
-}
+}*/
---
+++ javascript/crossFading.js
@@ -0,0 +1,18 @@
+/*var selected = 0
+
+$(document).ready(function() {
+  $("#fading").on('click', function() {
+    let count = $("#fading img").length;
+    selected += 1
+    if (selected < count) {
+      // Put all the images as transparent
+      $("#fading img").removeClass("opaque");
+      // Put only the selected image as opaque
+      $("#fading img").eq(selected).addClass("opaque");
+
+      // Same with the form
+      $("#").removeClass("opaque");
+      $(this).addClass("opaque");
+    }
+  });
+});*/
