cffdb74ce9c57c22201f2e94966f52873dac24b4
Romain Gehrig <romain.gehrig@gmail.com>
2018-05-09 01:13:24 +0200

    Quick save before the thunderstorm

--- javascript/button.js
+++ javascript/button.js
@@ -129,7 +129,7 @@ function process(social_increment, civil_increment, next_scene) {
 function updateText_d2_At_school() {
     let txt = "";
     if (talked_to_Mona) {
-        txt = "De retour à l'université le lendemain, Julien croise Mona dans un couloir. Celle-ci, contente de le voir, le salue chaleureusement et les deux discutent quelques minutes, avant d'avoir leur attention attirée par un des écrans d'information. Il arborait un poster flamboyante présentant l'équipe de basket. << Recherche de nouvelles recrues talentueuses ! >> tonnait l'affiche. Julien, les yeux pétillants d'excitation, avoue qu'il pensait justement rejoindre l'équipe parce qu'il en avait fait plus jeune à un niveau relativement élevé et qu'il aimait profondément l'énergie explosive des matchs de basket. Enthousiaste elle aussi, Mona l'encourage à aller voir l'entraîneur lors de la session de midi pour rencontrer l'équipe et participer à l'échauffement. Julien la remercie pour son idée et file en cours.<br /><br />";
+        txt = "De retour à l'université le lendemain, Julien croise Mona sur son banc. Celle-ci, contente de le voir, le salue chaleureusement et lui fait signe d'approcher.";
     }
     else {
         txt = "De retour à l'université le lendemain, Julien voit Mona de l'autre côté de la cour. Il croise son regard, qui semble vidé d'émotion. Un poster flamboyante présentant l'équipe de basket détourne son attention. << Recherche de nouvelles recrues talentueuses ! >> tonnait l'affiche. Julien sent une bouffée d'excitation s'emparer de lui. Il pensait justement rejoindre l'équipe de basket, en ayant fait plus jeune à un niveau relativement élevé et appréciant profondément ce sport. L'affiche précise de venir à un des entraînements de midi pour faire connaissance et participer à l'échauffement.<br /><br />";
@@ -140,49 +140,38 @@ function updateText_d2_At_school() {
 
 function updateText_d2_At_health_insurance() {
 
-    let text = "<b>TODO in JS</b><br/><br/>"
-    text += "Après les cours, Julien se rend dans une compagnie d'assurance pour contracter une assurance santé, mais on lui refuse l'assurance pour les raisons suivantes :<br /><br />"
+    let text = "";
+    text += "Julien arrive haletant dans la compagnie d'assurance la plus proche. Arrivé à l'accueil, il demande des renseignements pour contracter une assurance santé et on le redirige vers un vieil agent à l'air sec qui prendra en charge son dossier. Après s'être assis et avoir prouvé son identité, Julien observe l'agent pianoter sur son clavier avant de se retourner vers lui. D'un ton monotone celui-ci déclare que l'assurance refuse son dossier et en énumère les raisons: <br/><br/>";
 
     if (dna_stored) {
-        text += "  - L'ADN que Julien avait envoyé précédemment indique des probabilités de maladie supérieures à la moyenne.<br />"
+        text += "  - L'ADN que Julien avait envoyé précédemment indique des probabilités de maladie supérieures à la moyenne.<br />";
     }
     else {
-        text += "  - Aucune donnée sur l'ADN de Julien, mais l'ADN des parents indique des probabilités de maladie supérieures à la moyenne, Julien est donc à risque.<br />"
+        text += "  - Aucune donnée sur l'ADN de Julien, mais l'ADN des parents indique des probabilités de maladie supérieures à la moyenne, Julien est donc à risque.<br />";
     }
 
     if (fidelity_card) {
-        text += "  - Les données récoltées à partir du compte client que Julien avait créé pour obtenir sa carte de fidélité indiquent qu'il a acheté des aliments qui sont mauvais pour la santé. <br />"
+        text += "  - Les données récoltées à partir du compte client que Julien avait créé pour obtenir sa carte de fidélité indiquent qu'il a acheté des aliments qui sont mauvais pour la santé. <br />";
     }
     else {
-        text += "  - Aucune donnée sur les aliments que Julien achète.<br />"
+        text += "  - Aucune donnée sur les aliments que Julien achète.<br />";
     }
 
     if (ran) {
-        text += "  - Les données récoltées à partir de l'application que Julien avait utilisé pour aller courir indiquent que ses performances sont assez faibles. Le rythme cardiaque était également particulièrement élevé lors de la course, indicateur de mauvaise santé physique.<br />"
+        text += "  - Les données récoltées à partir de l'application que Julien avait utilisé pour aller courir indiquent que ses performances sont assez faibles. Le rythme cardiaque était également particulièrement élevé lors de la course, indicateur de mauvaise santé physique.<br />";
     }
     else {
-        text += "  - Les données récoltées à partir du GPS du téléphone de Julien indiquent qu'il se déplace peu, et donc qu'il fait très peu de sport.<br />"
+        text += "  - Les données récoltées à partir du GPS du téléphone de Julien indiquent qu'il se déplace peu, et donc qu'il fait très peu de sport.<br />";
     }
 
-    text += "<br />L'assureur dit à Julien que si il souhaite vraiment obtenir une assurance santé, alors le prix sera nettement plus élevé à cause des facteurs de risque. Julien est révolté, il n'est pas capable de payer une telle somme. Il s'énerve sur le vendeur en criant que c'est complètement stupide puisqu'il veut se faire assurer justement pour faire du basket, ce qui serait certainement bon pour sa santé. La colère de Julien est enregistrée par une caméra de surveillance qui se trouve à l'intérieur de la pièce."
-    document.getElementById("d2_At_health_insurance_text").innerHTML = text
-}
-
-function updateText_d2_Way_back_home() {
-    let text = "<b>TODO JS</b><br/><br/>Julien finit par sortir du bâtiment de la compagnie d'assurance. Il n'a pas pu se faire assurer et espère qu'il y aura une possibilité de jouer au basket malgré tout. Il se dirige vers la station de métro pour rentrer chez lui. "
-    if (civil_score < 2) {
-        text+= "Malheureusement, son score civil est trop bas, et lors du contrôle par reconnaissance faciale, le système lui interdit l'accès au métro. Les écarts de conduite de Julien, comme le fait de traverser un passage piéton alors que le feu est rouge, ou encore de s'être énerver contre l'agent de la compagnie d'assurance, a certainement dû contribuer à ce mauvais score. Décidément, c'est une très mauvaise journée pour Julien qui est alors forcé de rentrer chez lui à pied."
-    }
-    else {
-        text+= "Il se fait contrôler par le système de reconnaissance faciale, mais avant de pouvoir passer, le système avertit Julien que son score civil est très bas, et que si il continue à descendre, l'accès aux systèmes de transport en commun lui sera refusé. Julien se dit que c'est peut-être dû au fait qu'il a traversé un passage piéton alors que le feu était au rouge."
-    }
-    document.getElementById("d2_Way_back_home_text").innerHTML = text
+    text += "<br />L'agent avertit Julien que s'il souhaite vraiment obtenir une assurance santé, le prix sera nettement plus élevé pour prendre en compte les facteurs de risque. Julien, choqué, se rend compte il n'est pas capable de payer une telle somme et qu'il doit abandonner le basket. Une fois remis du choc, il sent une colère bouillonnante monter en lui: << Si je veux me faire assurer, c'est justement pour pour pouvoir faire du sport et par conséquent diminuer mes risques, pourquoi vous ne me laissez pas le faire ! >> fulmine-t-il à l'agent en tapant du poing sur la table. La sécurité est appelée mais Julien sort en coup de vent qu'elle n'ai eu le temps d'arriver.<br /><br />Une caméra à l'intérieur de la pièce a enregistré la scène.";
+    document.getElementById("d2_At_health_insurance_text").innerHTML = text;
 }
 
 function updateText_d2_At_home() {
-    let text = "<b>TODO JS</b><br/><br/> Le soir, comme à son habitude, Julien joue à des jeux de réflexion sur son smartphone. "
+    let text = "<b>TODO JS</b><br/><br/> Le soir, comme à son habitude, Julien joue à des jeux de réflexion sur son smartphone. ";
     if (stopped_when_Mona_sad) {
-        text+= "Il reçoit alors un message d'un ami de l'université qui l'a vu avec Mona et qui lui demande pourquoi il perd son temps avec une fille comme ça. Julien est assez surpris, pour lui ça n'a aucune importance de savoir avec qui il parle."
+        text+= "Il reçoit alors un message d'un ami de l'université qui l'a vu avec Mona et qui lui demande pourquoi il perd son temps avec une fille comme ça. Julien est assez surpris, pour lui ça ne regarde personne de savoir avec qui il parle.";
     }
     document.getElementById("d2_At_home_text").innerHTML = text
 }
